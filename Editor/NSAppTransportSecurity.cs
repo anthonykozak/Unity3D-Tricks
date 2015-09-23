b4b7ebf4928a5e892f4c0c6f86613d4bad0fdda3
Anthony Kozak <anthonykozak@gmail.com>
2015-09-23 11:51:47 +0200

    added NSAppTransportSecurity

---
+++ Editor/NSAppTransportSecurity.cs
@@ -0,0 +1,41 @@
+/**
+	@author : Anthony KOZAK :: exoa.fr
+	@description : Add your domain to the NSAppTransportSecurity for ios 9 in Unity
+		Add it automatically or manually in a custom editor menu.
+**/
+using System;
+using UnityEditor;
+using UnityEngine;
+using System.Collections.Generic;
+using System.IO;
+
+public class NSAppTransportSecurity
+{
+	[MenuItem("Tools/Build/Add NSAppTransportSecurity")]
+	private static void AddNSAppTransportSecurity()
+	{
+		string domain = "yourdomain.com";
+		string filepath = Application.dataPath.Replace("/Assets", "/") + "_BUILDS/ios/build/Info.plist";
+
+		if (File.Exists(filepath))
+		{
+			StreamReader streamReader = new StreamReader(filepath);
+			string text = streamReader.ReadToEnd();
+			streamReader.Close();
+			if (text.IndexOf("NSAppTransportSecurity") < 1)
+			{
+				text = text.Replace("<key>CFBundleDevelopmentRegion</key>", "<key>NSAppTransportSecurity</key>\n<dict>\n<key>NSExceptionDomains</key>\n<dict>\n<key>" + domain + "</key>\n<dict>\n<key>NSIncludesSubdomains</key>\n<false/>\n<key>NSExceptionAllowsInsecureHTTPLoads</key>\n<false/>\n<key>NSExceptionRequiresForwardSecrecy</key>\n<true/>\n<key>NSExceptionMinimumTLSVersion</key>\n<string>TLSv1.2</string>\n<key>NSThirdPartyExceptionAllowsInsecureHTTPLoads</key>\n<false/>\n<key>NSThirdPartyExceptionRequiresForwardSecrecy</key>\n<true/>\n<key>NSThirdPartyExceptionMinimumTLSVersion</key>\n<string>TLSv1.2</string>\n<key>NSRequiresCertificateTransparency</key>\n<false/>\n</dict>\n</dict>\n</dict>\n<key>CFBundleDevelopmentRegion</key>");
+				File.WriteAllText(filepath, text);
+				Debug.Log("NSAppTransportSecurity Added for domain " + domain);
+			}
+
+		}
+	}
+
+	[PostProcessBuild(1080)]
+	public static void OnPostProcessBuild(BuildTarget target, string path)
+	{
+		print("OnPostProcessBuild " + target + " " + path);
+		AddNSAppTransportSecurity();
+    }
+}
