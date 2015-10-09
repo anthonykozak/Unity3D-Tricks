2b90fd2624bd7c3ad5c1eb52e716495f88c94f14
Anthony Kozak <anthonykozak@gmail.com>
2015-10-09 16:18:17 +0200

    Added build and run script

---
+++ Editor/BuildAndRun.cs
@@ -0,0 +1,53 @@
+﻿/**
+	@author : Anthony KOZAK :: exoa.fr
+	@description : Build and RUn your desktop release using code.
+**/
+using UnityEditor;
+using UnityEngine;
+using System.Diagnostics;
+using System.Collections.Generic;
+
+public class BuildAndRun 
+{
+	private static string[] allScenes;
+
+	[MenuItem("Exoa/Tools/Build And Run Exe %&b")]
+	public static void BuildGame()
+	{
+		GatherSceneInfo();
+		// Get filename.
+		string fullPath = Application.dataPath.Replace("/Assets", "/") + "_BUILDS/win/build.exe";
+		
+		// Build player.
+		BuildPipeline.BuildPlayer(allScenes, fullPath, BuildTarget.StandaloneWindows, BuildOptions.None);
+		RunGame();
+	}
+	[MenuItem("Exoa/Tools/Run Exe %&r")]
+	public static void RunGame()
+	{
+		string fullPath = Application.dataPath.Replace("/Assets", "/") + "_BUILDS/win/build.exe";
+		
+		// Run the game (Process class from System.Diagnostics).
+		Process proc = new Process();
+		proc.StartInfo.FileName = fullPath;
+		proc.Start();
+	}
+
+
+	private static void GatherSceneInfo()
+	{
+		List<string> list = new List<string>();
+		EditorBuildSettingsScene[] scenes = EditorBuildSettings.scenes;
+
+		for (int iLevels = 0; iLevels < scenes.Length; iLevels++)
+		{
+			EditorBuildSettingsScene scene = scenes[iLevels];
+			if (true == scene.enabled)
+			{
+				list.Add(scene.path);
+			}
+		}
+		allScenes = list.ToArray();
+		UnityEngine.Debug.Log(allScenes);
+	}
+}
