ff94491d195eb1359526109fc1d74b7663658186
Anthony Kozak <anthonykozak@gmail.com>
2015-10-21 20:43:13 +0200

    Improving Sign n Package script

--- ExternalScripts/SignAndPackage.pl
+++ ExternalScripts/SignAndPackage.pl
@@ -1,8 +1,9 @@
 #!/usr/bin/perl
 use File::Copy;
+use File::Find;
 use Cwd;
 
-# Exoa SignAndPackage script v1.0
+# Exoa SignAndPackage script v1.1
 # Author : Anthony Kozak :: exoa.fr
 # Place this script in the same folder as the generated .app file from Unity
 # YOU WOULD NEED IN THIS DIRECTOY:
@@ -15,11 +16,14 @@ use Cwd;
 
 logit("Hello !");
 
-my $appName = ask("What's the .app name in this directory ?","build");;
+
+
+my $app_found = found_app_indir();
+my $appName = ask("What's the .app name in this directory ?", $app_found);
 my $appPath = getcwd . "/".$appName.".app";
 my $appType = ask("Is this app for the MacAppStore or External ?","MacAppStore");
-my $appPathSigned = getcwd . "/".$appName."_".$appType."_signed.app";
-my $packagePath = getcwd . "/".$appName."_".$appType.".pkg";
+my $appPathSigned = getcwd . "/".$appType."/".$appName.".app";
+my $packagePath = getcwd . "/".$appType."/".$appName.".pkg";
 
 
 
@@ -28,7 +32,7 @@ my $doCodeSigning = ask("Sign the app ?", "true");
 my $doCreatePackage = ask("Generate package ?","true");
 my $copyInfopList = ask("Copy Info.plist from this directory inside the .app ?","true");
 my $copyIcons = ask("Copy PlayerIcon.icns from this directory inside the .app ?","false");
-my $copyIcon = ask("Copy UnityPlayerIcon.png from this directory inside the .app ?","false");
+my $copyIcon = ask("Copy UnityPlayerIcon.png from this directory inside the .app ?","true");
 
 my $srcAssetPath = "/";
 
@@ -52,17 +56,15 @@ logit("*** Starting Process - Building at '$appPath' ***");
 my $src = "";
 my $dest = "";
 
-# this copies appPath to appPathSigned and use it
-print ("\n*** Copying " . $appPath . " to " . $appPathSigned);
-system("cp -r \"".$appPath."\" \"".$appPathSigned."\"");
-
+# Creating target dir
+mkdir $appType, 0755;
 
 # this copies your own /Info.plist to the generated game
 if($copyInfopList eq "true")
 {
     $plist = getcwd . $srcAssetPath . "Info.plist";
     $dest = $appPath . "/Contents/Info.plist";
-    print ("\n*** Copying " . $plist . " to " . $dest);
+    print ("\n*** Copying " . getShort($plist). " to " . getShort($dest));
     copy($plist, $dest) or die "File can not be copied: " . $plist;
 }
     
@@ -79,15 +81,18 @@ if($copyIcon eq "true")
 {
     $playericon = getcwd . $srcAssetPath . "UnityPlayerIcon.png";
     $dest = $appPath . "/Contents/Resources/UnityPlayerIcon.png";
-    print ("\n*** Copying " . $playericon . " to " . $dest);
+    print ("\n*** Copying " . getShort($playericon) . " to " . getShort($dest));
     copy($playericon, $dest) or die "File can not be copied: " . $playericon;
 }
 # this copies $profile to your generated game 
 $src = getcwd . $srcAssetPath . $profile;
 $dest = $appPath . "/Contents/embedded.provisionprofile";
-print ("\n*** Copying " . $src . " to " . $dest);
+print ("\n*** Copying " . getShort($src) . " to " . getShort($dest));
 copy($src, $dest) or die "File can not be copied: " . $src;
 
+# this copies appPath to appPathSigned and use it
+print ("\n*** Copying " . getShort($appPath) . " to " . getShort($appPathSigned));
+system("cp -r \"".$appPath."\" \"".$appPathSigned."\"");
 
 #fail safe check: If accidentially signing is set to false, but packaging set to true, signing is turned on to create a valid package.
 if ($doCreatePackage eq "true") {
@@ -96,17 +101,18 @@ if ($doCreatePackage eq "true") {
 
 ## Chmod and remove unecessary files
 system("/bin/chmod -R a+rwx \"$appPathSigned\"");
-system("find \"$appPathSigned\" -name \*.meta -exec rm -r \"{{}}\" \\;");
+system("find \"$appPathSigned\" -name \*.meta -exec rm -r \"{}\" \\;");
 system("/usr/sbin/chown -RH \"cestdesbullshit1:staff\" \"$appPathSigned\"");
 system("/bin/chmod -RH u+w,go-w,a+rX \"$appPathSigned\"");
 my $CodesignEnvironment = $ENV{'CODESIGN_ALLOCATE'};
 $ENV{'CODESIGN_ALLOCATE'}="/Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/codesign_allocate";
-recursiveCodesign("$appPathSigned/Contents/Frameworks");
-recursiveCodesign("$appPathSigned/Contents/Plugins");
 
 
 # Auto code signing
 if ($doCodeSigning eq "true") {
+	recursiveCodesign("$appPathSigned/Contents/Frameworks");
+	recursiveCodesign("$appPathSigned/Contents/Plugins");
+	recursiveCodesign("$appPathSigned/Contents/MacOS");
 	logit ("*** Start signing");
 	system("/usr/bin/codesign --force --timestamp=none --sign \"" . $certificateApplication . "\" ".$entitlementsFileName." \"" . $appPathSigned . "\"");
 	logit ("*** Verify signing");
@@ -115,7 +121,7 @@ if ($doCodeSigning eq "true") {
 
 # Auto creating a package file?
 if ($doCreatePackage eq "true") {
-    logit("*** Start packaging");
+	logit("*** Start packaging");
 	system("productbuild --component \"" . $appPathSigned . "\" /Applications --sign \"". $certificateInstaller . "\" --product \"$appPathSigned/Contents/Info.plist\" \"" . $packagePath . "\"");
 }
 $ENV{'CODESIGN_ALLOCATE'}=$CodesignEnvironment;
@@ -137,18 +143,37 @@ sub logit{
 sub recursiveCodesign {
 
     my $dirName = shift;
-    print("\n*** Recursive Codesigning ".$dirName."\n");
+    print("\n*** Recursive Codesigning ".getShort($dirName)."\n");
     opendir my($dh), $dirName or return;
     my @files = readdir($dh);
     closedir $dh;
     foreach my $currentFile (@files) {
         next if $currentFile =~ /^\.{1,2}$/;
         if ( lc($currentFile) =~ /.bundle$/ or lc($currentFile) =~ /.dylib$/ or lc($currentFile) =~ /.a$/ or lc($currentFile) =~ /.so$/ or lc($currentFile) =~ /.lib$/ or (-f "$dirName/$currentFile" && $currentFile =~ /^[^.]*$/ && `file "$dirName/$currentFile"` =~ /Mach-O/) ) {
-            print("\tCodesigning ".$dirName."/".$currentFile."\n");
+            print("\tCodesigning ".getShort($currentFile)."\n");
             system("/usr/bin/codesign --force --timestamp=none --sign \"".$certificateApplication."\" \"$dirName/$currentFile\"");
         }
         if (-d "$dirName/$currentFile") {
              recursiveCodesign("$dirName/$currentFile");
         }
     }
+}
+sub found_app_indir()
+{
+	opendir(my $dh, '.') || die "cant open dir";
+	my @list_found = grep { /.app$/ } readdir($dh);
+	my $app_found = @list_found[0];
+	chop($app_found); chop($app_found);chop($app_found); chop($app_found);
+	return $app_found;
+}
+sub getShort() {
+	my $subject = shift;
+	my $search = getcwd . "/";
+	my $replace = "";
+	my $pos = index($subject, $search);
+	while($pos > -1) {
+		substr($subject, $pos, length($search), $replace);
+		$pos = index($subject, $search, $pos + length($replace));
+	}
+	return $subject;
 }
